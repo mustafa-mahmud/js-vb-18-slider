71c26240441dbdd8d5202280c7077bdb59af5ca0
mustafa-mahmud <mithuweb000@gmail.com>
2021-05-03 10:03:53 +0600

    End Project

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const images = document.querySelectorAll('.slider img');
+const btnRight = document.querySelector('.right');
+const btnLeft = document.querySelector('.left');
+
+let currentIndex = 0;
+
+slider(currentIndex);
+
+function slider(currentIndex) {
+  images.forEach((img, ind) => {
+    img.style.transform = `translateX(${100 * (ind - currentIndex)}%)`;
+  });
+}
+
+function prevSlide() {
+  if (currentIndex === images.length - 1) currentIndex = 0;
+  else currentIndex++;
+
+  slider(currentIndex);
+}
+
+function nextSlide() {
+  if (currentIndex === 0) currentIndex = images.length - 1;
+  else currentIndex--;
+
+  slider(currentIndex);
+}
+
+btnRight.addEventListener('click', prevSlide);
+
+btnLeft.addEventListener('click', nextSlide);
